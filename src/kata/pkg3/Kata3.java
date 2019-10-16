978b80b45c4a580f393fd3c35062d2385dede4ee
Usuario <Usuario@10.22.148.82>
2019-10-16 09:46:06 +0100

    Kata 2 using a HistogramDisplay interface

--- src/kata/pkg3/Kata3.java
+++ src/kata/pkg3/Kata3.java
@@ -1,21 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kata.pkg3;
 
-/**
- *
- * @author Usuario
- */
+
 public class Kata3 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+     public static void main(String[] args) {
+         
+         Histogram<String> histogram = new Histogram<>();
+         histogram.increment("gmail.com");
+         histogram.increment("ulpgc.es");
+         histogram.increment("ulpgc.es");
+         histogram.increment("dis.ulpgc.es");
+         histogram.increment("ulpgc.es");
+         histogram.increment("gmail.com");
+         histogram.increment("dis.ulpgc.es");
+         histogram.increment("gmail.com");
+         
+         
+         
+         
+         HistogramDisplay histoDisplay= new HistogramDisplay(histogram);
+         histoDisplay.execute();
+     }
     
 }
